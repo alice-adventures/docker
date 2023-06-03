3203388084b77a4ff7ae21df93a23487846f888d
Francesc Rocher <francesc.rocher@gmail.com>
2023-06-03 18:00:41 +0200

    Fix arguments

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -3,4 +3,4 @@
 sudo --preserve-env nginx
 sudo --preserve-env php-fpm${PHP_FPM_VERSION}
 [ -n "$(which code-server)" ] && start-code-server.sh
-ttyd --port 8086 --cwd /home/ada/alice/ --max-clients 0 -url-arg -client-option fontSize=15 -client-option lineHeight=1.1 bash
+ttyd --port 8086 --cwd /home/ada/alice/ --max-clients 0 --url-arg --client-option fontSize=15 --client-option lineHeight=1.1 bash
