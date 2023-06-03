f3ddc3a54e42f38f5cecb1d909c07a12537c9774
Francesc Rocher <francesc.rocher@gmail.com>
2023-06-03 20:52:58 +0200

    Update scripts

---
+++ assets/alire/update-alire.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+RELEASES_URL="https://api.github.com/repos/alire-project/alire/releases"
+
+# check latest version
+LATEST_RELEASE_URL=$( \
+      curl -L $RELEASES_URL 2>/dev/null \
+    | grep 'browser_download_url.*\.zip' \
+    | grep -v 'nightly' \
+    | head -1 \
+    | sed -e 's_.*\(http.*\.zip\).*_\1_')
+LATEST_RELEASE_FILE=$(basename $LATEST_RELEASE_URL)
+
+cd /tmp
+
+# download latest version
+if [ ! -f $LATEST_RELEASE_FILE ]; then
+    sudo rm -f alr-*.zip
+    curl -fOL $LATEST_RELEASE_URL
+    if [ ! $? -eq 0 ]; then
+        echo "Error: could not download $LATEST_RELEASE_URL"
+    fi
+fi
+
+# install latest version
+if [ -f $LATEST_RELEASE_FILE ]; then
+    unzip $LATEST_RELEASE_FILE
+    sudo mv bin/alr /usr/local/bin/alr
+fi
--- assets/code-server/update-code-server.sh
+++ assets/code-server/update-code-server.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-LATEST_RELEASE_URL=$(curl -L https://api.github.com/repos/coder/code-server/releases 2>/dev/null \
-              | grep 'browser_download_url.*_amd64\.deb' \
-              | grep -v '\-rc\.[0-9]*/' \
-              | head -1 \
-              | sed -e 's_.*\(http.*\.deb\).*_\1_')
+RELEASES_URL="https://api.github.com/repos/coder/code-server/releases"
+
+# check latest version
+LATEST_RELEASE_URL=$( \
+      curl -L $RELEASES_URL 2>/dev/null \
+    | grep 'browser_download_url.*_amd64\.deb' \
+    | grep -v '\-rc\.[0-9]*/' \
+    | head -1 \
+    | sed -e 's_.*\(http.*\.deb\).*_\1_')
 LATEST_RELEASE_FILE=$(basename $LATEST_RELEASE_URL)
 
 cd /tmp
 
+# download latest version
 if [ ! -f $LATEST_RELEASE_FILE ]; then
-    rm -f code-server*
-    curl -fOL ${LATEST_RELEASE_URL}
+    sudo rm -f code-server*
+    curl -fOL $LATEST_RELEASE_URL
     if [ ! $? -eq 0 ]; then
         echo "Error: could not download $LATEST_RELEASE_URL"
     fi
 fi
 
+# install latest version
 if [ -f $LATEST_RELEASE_FILE ]; then
-    apt install -y ./$LATEST_RELEASE_FILE
+    sudo apt install -y ./$LATEST_RELEASE_FILE
 
     code-server --install-extension Adacore.ada
     code-server --install-extension yzhang.markdown-all-in-one
