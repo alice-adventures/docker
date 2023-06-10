772cd6db947af8618bb08978a2ecdee7434bc7f7
Francesc Rocher <francesc.rocher@gmail.com>
2023-06-10 11:07:18 +0200

    Import env when testing

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -f /usr/local/bin/alice-test.sh ]; then
+    echo "Loading alice-test environment"
+    for v in $(env | sed -e 's:^\([^=]*\)=.*$:\1:'); do unset ${v}; done
+    source /usr/local/bin/alice-test.sh
+    env
+    cd /home/ada/alice
+fi
+
 [ -n "$(which code-server)" ] && start-code-server.sh
 
 start-nginx.sh
